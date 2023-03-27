d33822eb4b33e6866b8a6ce38fe875ddfb6a7d38
Christopher Kennedy <kennednu@bc.edu>
2023-03-26 21:10:48 -0400

    Initial Commit

---
+++ Snacktacular/SnacktacularApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularApp.swift
+//  Snacktacular
+//
+//  Created by Christopher Kennedy on 3/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
