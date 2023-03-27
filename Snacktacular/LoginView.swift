4779bb1e284d3e7f7966828b00dba0e72e324bbc
Christopher Kennedy <kennednu@bc.edu>
2023-03-26 22:17:07 -0400

    Ch. 8.3 Using the Keyboard to Move Between TextFields in SwiftUI (Snacktacular app)

--- Snacktacular/LoginView.swift
+++ Snacktacular/LoginView.swift
@@ -9,10 +9,17 @@ import SwiftUI
 import Firebase
 
 struct LoginView: View {
+    enum Field{
+        case email, password
+    }
+    
     @State private var email = ""
     @State private var password = ""
     @State private var showingAlert = false
     @State private var alertMessage = ""
+    @State private var buttonDisable = true
+    @FocusState private var focusField: Field?
+    
     var body: some View {
         NavigationStack{
             Image("logo")
@@ -25,10 +32,26 @@ struct LoginView: View {
                     .autocorrectionDisabled()
                     .textInputAutocapitalization(.never)
                     .submitLabel(.next)
+                    .focused($focusField, equals: .email)
+                    .onSubmit {
+                        focusField = .password
+                    }
+                    .onChange(of: email) { _ in
+                        enableButtons()
+                    }
+                
                 SecureField("Password", text: $password)
                     .textInputAutocapitalization(.never)
                     .submitLabel(.done)
+                    .focused($focusField, equals: .password)
+                    .onSubmit {
+                        focusField = nil
+                    }
+                    .onChange(of: password) { _ in
+                        enableButtons()
+                    }
             }
+            .disabled(buttonDisable)
             .textFieldStyle(.roundedBorder)
             .overlay {
                 RoundedRectangle(cornerRadius: 5)
@@ -61,6 +84,13 @@ struct LoginView: View {
             Button("OK", role: .cancel) {}
         }
     }
+    
+    func enableButtons(){
+        let emailIsGood = email.count > 6 && email.contains("@")
+        let passwordIsGood = password.count > 6
+        buttonDisable = !(emailIsGood && passwordIsGood)
+    }
+    
     func register(){
         Auth.auth().createUser(withEmail: email, password: password){
             result, error in
